11899ae774d2398570e9136a31aa63331b836b51
noctisye <noctisye@tencent.com>
2025-01-17 16:32:19 +0800

    ItemManager added

---
+++ UGCItemFramework/Source/UGCItemFramework/ItemManager.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "Item.h"
+#include "ItemManager.generated.h"
+UCLASS()
+class AItemManager : public AActor
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "ItemManager")
+	TArray<AItem*> ItemPool;
+};
