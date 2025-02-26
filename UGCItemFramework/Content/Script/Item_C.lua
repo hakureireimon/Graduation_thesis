f0a8cee9500631f8b64d19be5933b92b4a7a2abf
noctisye <noctisye@tencent.com>
2025-02-26 14:33:16 +0800

    Item overlap delegate bind success

--- UGCItemFramework/Content/Script/Item_C.lua
+++ UGCItemFramework/Content/Script/Item_C.lua
@@ -1,3 +1,11 @@
 local M = UnLua.Class()
 
+function M:ReceiveBeginPlay()
+    self.EventOnItemPickedUp:Add(self, self.ItemPickedUp)
+end
+
+function M:ItemPickedUp()
+    print('overlap bind success')
+end
+
 return M
