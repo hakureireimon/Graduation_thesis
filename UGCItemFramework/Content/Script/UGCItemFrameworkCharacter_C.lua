7b2ff4824c4ef319d489e120002bcacedc39d866
noctisye <noctisye@tencent.com>
2025-03-11 20:25:59 +0800

    E键事件添加

--- UGCItemFramework/Content/Script/UGCItemFrameworkCharacter_C.lua
+++ UGCItemFramework/Content/Script/UGCItemFrameworkCharacter_C.lua
@@ -3,6 +3,10 @@ local BindKey = UnLua.Input.BindKey
 
 BindKey(M, "F", "Pressed", UE.AUGCItemFrameworkCharacter.OnFPressed)
 BindKey(M, "B", "Pressed", UE.AUGCItemFrameworkCharacter.ShowOrHideBag)
+BindKey(M, "E", "Pressed", function(self)
+    local EffectManager = UE.UGameplayStatics.GetActorOfClass(self:GetWorld(), UE.AEffectManager)
+    EffectManager:SendSignal('EPressed', "extra:666")
+end)
 
 function M:ReceiveBeginPlay()
     self.OnGenerateItem:Add(self, self.test)
@@ -11,11 +15,6 @@ end
 function M:test()
     local EffectManager = UE.UGameplayStatics.GetActorOfClass(self:GetWorld(), UE.AEffectManager)
     EffectManager:SendSignal('ItemGenerated', "extra:666")
-end
-
-function M:OnEPressed()
-    local EffectManager = UE.UGameplayStatics.GetActorOfClass(self:GetWorld(), UE.AEffectManager)
-    EffectManager:SendSignal('EPressed', "extra:666")
 end
 
 return M
