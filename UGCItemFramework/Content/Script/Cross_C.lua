9a24a23a47e51745adf4b65e803671fdf0409190
noctisye <noctisye@tencent.com>
2025-03-12 15:04:07 +0800

    暂存（准心Lua化，但没有想好做什么效果）

---
+++ UGCItemFramework/Content/Script/Cross_C.lua
@@ -0,0 +1,23 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+local M = UnLua.Class()
+
+function M:Initialize(Initializer)
+    self.Cross = self.TextBlock_0
+end
+
+--function M:PreConstruct(IsDesignTime)
+--end
+
+-- function M:Construct()
+-- end
+
+-- function M:Tick(MyGeometry, InDeltaTime)
+-- end
+
+return M
